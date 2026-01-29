a0da218524d6ed36543d267da9d949913e46c6c7
molya-polat <65759704+molya-polat@users.noreply.github.com>
2026-01-29 15:34:33 +0000

    LeetCode Synchronization - Reverse String (Python3)

---
+++ problems/0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        for i in range(n // 2):
+            tmp = s[i]
+            s[i] = s[n - i - 1]
+            s[n - i - 1] = tmp
+
+            # O(n) - time
+            # O(1) - space
